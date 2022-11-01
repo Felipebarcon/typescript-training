266d8543d0d9d0e2a97537144e69305b39cc1fd9
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-01 17:50:48 +0100

    functions in typescript

--- lesson1-introduction/src/index.ts
+++ lesson1-introduction/src/index.ts
@@ -32,6 +32,41 @@ console.log(add(1, 2));
 let add1: (nbr1: number, nbr2: number) => number;
 let add2: Function;
 
-add1 = (a: number, b: number) => {
+add1 = (a: number, b?: number) => {
   return a + b;
 };
+
+console.log(add1(5, 2));
+
+console.log("********************************");
+
+let add3 = (...numbers: Array<number>) => {
+  return numbers.reduce((acc, i) => {
+    console.log(i);
+    acc += i;
+    return acc;
+  }, 0);
+};
+
+console.log("***********TOTAL**************");
+
+console.log(add3(1, 2, 3, 4));
+
+function add4(a, b): string | number {
+  return a + b;
+}
+
+function add5(a: number, b: number): number;
+function add5(a: string, b: string): string;
+function add5(a, b): string | number {
+  return a + b;
+}
+const res1 = add5(1, 2);
+const res2 = add5("hello ", "world");
+
+res2.split("");
+console.log(res2.length);
+
+//*****************************************//
+//*****************************************//
+// CLASSES
