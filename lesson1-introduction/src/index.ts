d827c310a67ddf7591976e625756cc10ebc44ebc
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-02 16:15:51 +0100

    Classes génériques

--- lesson1-introduction/src/index.ts
+++ lesson1-introduction/src/index.ts
@@ -52,5 +52,43 @@ const addItemToCollection: AddItemFunction = <T>(
 const myFruits = addItemToCollection(newFruit, []);
 const myCars = addItemToCollection(newCar, []);
 
-console.log(myFruits[0].price);
-console.log(myCars[0].speed);
+// console.log(myFruits[0].price);
+// console.log(myCars[0].speed);
+
+//*****************************************//
+//*****************************************//
+// CLASSE ET TYPES GENERIQUES
+
+class Stack<T> {
+  items: T[] = [];
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): T {
+    return this.items.pop();
+  }
+
+  display(): void {
+    console.log(this.items);
+  }
+}
+
+const newStackNumber = new Stack<number>();
+newStackNumber.push(1);
+newStackNumber.display();
+newStackNumber.push(2);
+newStackNumber.display();
+const myNumber = newStackNumber.pop();
+newStackNumber.display();
+console.log(myNumber);
+
+const newStackString = new Stack<string>();
+newStackString.push("hello");
+newStackString.display();
+newStackString.push("world");
+newStackString.display();
+const myString = newStackString.pop();
+newStackString.display();
+console.log(myString);
