f464b029bebc783571f831cf4da101d7dfb65bbd
Felipe Barcon <felipe.barcon@gmail.com>
2022-11-04 14:08:31 +0100

    comtinue décorateur

--- lesson1-introduction/src/index.ts
+++ lesson1-introduction/src/index.ts
@@ -2,9 +2,14 @@
 //*****************************************//
 // DECORATEURS
 
-// function Component(target: any) {
-//   console.log(" LOG : Component MyComponent ");
-// }
+function Component(target: any) {
+  console.log(" LOG : Component MyComponent ");
+}
+
+function LogFactory() {
+  console.log("in  log factory");
+  return Component;
+}
 
 function ComponentFactory({
   selector,
@@ -13,22 +18,65 @@ function ComponentFactory({
   template: string;
   selector: string;
 }) {
+  console.log("in component factory");
   const elem = document.querySelector(selector);
   elem.innerHTML = template;
-  return (target: any) => {};
+  return (target: any) => {
+    console.log("decorator component");
+  };
 }
 
-@ComponentFactory({
-  template: "<h1>Hello</h1>",
-  selector: "app",
-})
+function Method(
+  target: any,
+  propName: string,
+  propertyDescriptor: PropertyDescriptor
+) {
+  return {
+    value: function () {
+      if (this.name) {
+        propertyDescriptor.value.call(this);
+      } else {
+        console.log("no name");
+      }
+    },
+  };
+}
+
+function Method2(
+  target: any,
+  propName: string,
+  propertyDescriptor: PropertyDescriptor
+) {
+  console.log({
+    target,
+    propName,
+    propertyDescriptor,
+  });
+}
+
+// @LogFactory()
+// @ComponentFactory({
+//   template: "<h1>Hello</h1>",
+//   selector: "app",
+// })
 class MyComponent {
   public name: string;
 
-  constructor(name: string) {
+  @Method
+  public greeting() {
+    console.log(`Hello: ${this.name}`);
+  }
+
+  @Method2
+  get fancyName() {
+    return `Hello Monsieur ${this.name}`;
+  }
+
+  constructor(name?: string) {
     this.name = name;
   }
 }
 
-const foo = new MyComponent("jean");
+const foo = new MyComponent("Jean");
 const bar = new MyComponent("marie");
+foo.greeting();
